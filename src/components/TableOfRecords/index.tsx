dcca22133d6d9423e0c2f8c73f6b080746a6ea9d
dowlsss <dowlssss@yandex.ru>
2023-02-24 22:03:16 +0500

    fix table

--- src/components/TableOfRecords/index.tsx
+++ src/components/TableOfRecords/index.tsx
@@ -2,11 +2,33 @@ import styles from "./style/index.module.scss";
 import {useAppDispatch} from "../../hooks/useRedux";
 import {mineSlice} from "../../redux/reducers/mineReducer";
 import {getPadTime} from "../Timer";
-import React from "react";
+import React, {useState} from "react";
 
 export const TableOfRecords = () => {
+    const [time, setTime] = useState(localStorage.getItem('time'))
     const dispatch = useAppDispatch();
     const {setOpenTableWithResults} = mineSlice.actions
+    const createTable = () => {
+        if (time === null) {
+            return <th/>;
+        }
+        try {
+            JSON.parse(localStorage.getItem('time') as string).map((value: number, index: number) => {
+                const min = getPadTime(Math.floor(value / 60));
+                const sec = getPadTime(value - Math.floor(value / 60) * 60);
+                return (
+                    <>
+                        <th key={index}>{index}</th>
+                        <th key={index + 1}><span>{min}</span>
+                            <span>:</span>
+                            <span>{sec}</span></th>
+                    </>
+                )
+            })
+        } catch (e) {
+            return <th/>;
+        }
+    }
     return (
         <div className={styles.container}>
             <button onClick={() => dispatch(setOpenTableWithResults())} className={styles.btn}>Menu</button>
@@ -20,18 +42,7 @@ export const TableOfRecords = () => {
                     </thead>
                     <tbody>
                     <tr>
-                        {JSON.parse(localStorage.getItem('time') as string).map((value: number, index: number) => {
-                            const min = getPadTime(Math.floor(value / 60));
-                            const sec = getPadTime(value - Math.floor(value / 60) * 60);
-                            return (
-                                <>
-                                    <th key={index}>{index}</th>
-                                    <th key={index + 1}><span>{min}</span>
-                                        <span>:</span>
-                                        <span>{sec}</span></th>
-                                </>
-                            )
-                        })}
+                        {createTable()}
                     </tr>
                     </tbody>
                 </table>
